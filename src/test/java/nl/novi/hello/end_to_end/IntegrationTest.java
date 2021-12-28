8c43ae32cddcd5bc6549f358d7f3d2383b8cb0c0
Peter Anema <email@peteranema.nl>
2021-12-28 10:46:13 +0100

    Added End-To-End Integration test with security.
    By specifying user in mockMvc.perform method.
    Chain get(url) method with 'with(user(...).roles(...)'

---
+++ src/test/java/nl/novi/hello/end_to_end/IntegrationTest.java
@@ -0,0 +1,38 @@
+package nl.novi.hello.end_to_end;
+
+import nl.novi.hello.HelloApplication;
+import nl.novi.hello.config.WebSecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = HelloApplication.class)
+@AutoConfigureMockMvc
+@EnableConfigurationProperties
+//@WithMockUser(username = "admin", roles = {"ADMIN"})
+public class IntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturn200ForEndpointUsers() throws Exception {
+        mockMvc.perform(get("/users").with(user("admin").roles("ADMIN")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturn200ForEndpointBooks() throws Exception {
+        mockMvc.perform(get("/books").with(user("user").roles("USER")))
+                .andExpect(status().isOk());
+    }
+
+}
